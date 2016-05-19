c327a69ed692416011d231c6892e161b8def91d3
David Wade Hagar <cyclops@blazingumbra.com>
2016-05-19 18:06:52 -0400

    Alright...  Lets Begin

---
+++ snowboard.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+# Snowboard - IRC Bot Written in Python 3
+# See:  https://github.com/dwhagar/snowboard for more information.
+
+def main():
+	result = 0	# Define a result value, so we can pass it back to the shell
+	
+	# Start here
+	
+	return result
+
+main()
