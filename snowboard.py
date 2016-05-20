698444ec4cefb8a222a7a9942a7ea112ae13467c
Saria Mithril <saria_mithril@hotmail.com>
2016-05-20 00:39:55 -0400

    Added support for command-line arguments

--- snowboard.py
+++ snowboard.py
@@ -3,11 +3,26 @@
 # Snowboard - IRC Bot Written in Python 3
 # See:  https://github.com/dwhagar/snowboard for more information.
 
-def main():
+import argparse
+import sys
+
+# Program arguments
+args = None
+
+def main(argv):
+	argparser = argparse.ArgumentParser(
+		prog="snowboard",
+		description="IRC Bot Written in Python 3.",
+		fromfile_prefix_chars="@")
+	
+	global args
+	args = argparser.parse_args(argv)
+	
 	result = 0	# Define a result value, so we can pass it back to the shell
 	
 	# Start here
 	
 	return result
 
-main()
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
