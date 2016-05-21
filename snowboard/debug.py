edb2ddce1de88bf0800c4f4a59927e066f862ad0
Saria <saria.mithril@gmail.com>
2016-05-21 19:16:29 -0400

    Added debug printing functions

--- snowboard/debug.py
+++ snowboard/debug.py
@@ -15,6 +15,87 @@
 
 import sys
 
+from . import config
+
 def print_message(*objects, sep=" ", end="\n", file=sys.stdout, flush=False, level=1, minlevel=1):
+    """Writes a message if the level is high enough.
+    
+    Works just like the standard print function, if and only if `level`
+    is at least `minlevel`. Otherwise it does nothing.
+    
+    Its primary purpose is as the underlying implementation for all the
+    output functions in the debug module.
+    
+    Parameters
+    ----------
+    objects : positional parameter pack of objects
+        Each object in `*objects` is converted to `str` and printed in turn.
+    sep : str
+        Value output between each item in `objects`.
+    end : str
+        Value output at the end.
+    file : file-like
+        File-like object to write output to.
+    flush : bool
+        If `True`, flush `file` after output.
+    level : int
+        Level of output.
+    minlevel : int
+        `level` must be at least `minlevel` for any output.
+    """
     if level >= minlevel:
         print(*objects, sep=sep, end=end, file=file, flush=flush)
+
+def message(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+    """Writes a message if `config.verbosity` is at least 1.
+    
+    Parameters
+    ----------
+    objects : positional parameter pack of objects
+        Each object in `*objects` is converted to `str` and printed in turn.
+    sep : str
+        Value output between each item in `objects`.
+    end : str
+        Value output at the end.
+    file : file-like
+        File-like object to write output to.
+    flush : bool
+        If `True`, flush `file` after output.
+    """
+    print_message(*objects, sep=sep, end=end, file=file, flush=flush, level=config.verbosity, minlevel=1)
+
+def info(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+    """Writes a message if `config.verbosity` is at least 2.
+    
+    Parameters
+    ----------
+    objects : positional parameter pack of objects
+        Each object in `*objects` is converted to `str` and printed in turn.
+    sep : str
+        Value output between each item in `objects`.
+    end : str
+        Value output at the end.
+    file : file-like
+        File-like object to write output to.
+    flush : bool
+        If `True`, flush `file` after output.
+    """
+    print_message(*objects, sep=sep, end=end, file=file, flush=flush, level=config.verbosity, minlevel=2)
+
+def trace(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+    """Writes a message if `config.verbosity` is at least 3.
+    
+    Parameters
+    ----------
+    objects : positional parameter pack of objects
+        Each object in `*objects` is converted to `str` and printed in turn.
+    sep : str
+        Value output between each item in `objects`.
+    end : str
+        Value output at the end.
+    file : file-like
+        File-like object to write output to.
+    flush : bool
+        If `True`, flush `file` after output.
+    """
+    print_message(*objects, sep=sep, end=end, file=file, flush=flush, level=config.verbosity, minlevel=3)
