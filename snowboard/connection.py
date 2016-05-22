185ff321ef1d019cc40d48f7ffd37d563ac3a823
dwhagar <cyclops@blazingumbra.com>
2016-05-21 20:51:32 -0400

    Connection (including SSL) not works and reads data.
    I still need to write the send data function and the IRC login function.

--- snowboard/connection.py
+++ snowboard/connection.py
@@ -1,5 +1,7 @@
 import time
 import socket
+import ssl
+import sys
 
 '''
 Connection object, designed to be the only object to directly interface with
@@ -10,8 +12,10 @@ class Connection:
     def __init__(self, host, port):
         self.__host = host
         self.__port = port
-        self.__socket = Socket()
+        self.__socket = None
         self.__connected = False
+        self.ssl = False
+        self.sslVerify = True
         self.error = ""            # Later this will contain the laste error
         self.retries = 3           # Numbers of times to retry a connection
         self.delay = 0.5           # Delay between connection attempts
@@ -35,19 +39,76 @@ class Connection:
         attempt = 0
         
         # Try until the connection succeeds or no more tries are left.
-        while self.__connected == False and attempt <= self.retries:
-            try:
-                self.__socket.create_connection((self.__host, self.__port))
-                self.__connected = True
-            except:
-                pass # TODO:  Put code here to identify unrecoverable
-                     #        conneciton problems.
-            # Delay a certain amount of time between attempts.
+        while (not self.__connected) and (attempt <= self.retries):
+            #try:
+            self.__socket = socket.create_connection((self.__host, self.__port))
+            
+            # Handle SSL
+            if self.ssl:
+                self.__context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+                if not self.sslVerify:
+                    self.__context.verify_mode = ssl.CERT_NONE
+                self.__ssl = self.__context.wrap_socket(self.__socket)
+                self.__ssl.setblocking(False)
+            # Handle not SSL
+            else:
+                pass
+                self.__socket.setblocking(False)
+            
+            self.__connected = True
+            self.error = ""
+            
+            #    break
+            #except:
+            #    self.error = "Unable to connect."
+            attempt += 1
+            
             time.sleep(self.delay)
         
         return self.__connected
     
+    # Disconnect the socket.
     def disconnect(self):
         self.__socket.close()
+        self.__socket = None
         self.__connected = False
     
+    # Read information from the server, up to a new-line character.
+    def read(self):
+        # Keep track on if the loop should be done.
+        done = False
+        received = ""
+        
+        # Only do something if we're connected.
+        if self.__connected:
+            while not done:
+                try:
+                    if self.ssl:
+                        data = self.__ssl.recv(1).decode()
+                    else:
+                        data = self.__socket.recv(1).decode()
+                except (ssl.SSLWantReadError, BlockingIOError):
+                    received = False
+                    break
+                
+                # Process the data.
+                # socket.recv is supposed to return a False if the connection
+                # been broken.
+                if not data:
+                    self.error = "Disconnected."
+                    self.disconnect()
+                    done = True
+                    received = False
+                elif data == '\n':
+                    done = True
+                else:
+                    received += data
+        else:
+            # Return false if the system is not connected.
+            received = False
+        
+        # Remove the trailing carriage return character (cr/lf pair)
+        if not type(received) == bool:
+            received = received.strip('\r')
+        
+        return received
