0f8475593333e6cfc54c2e57b0df203fbb78759a
dwhagar <cyclops@blazingumbra.com>
2016-05-22 09:57:07 -0400

    Added GNU to all files.

--- snowboard/channel.py
+++ snowboard/channel.py
@@ -1,3 +1,18 @@
+# This file is part of snowboard.
+# 
+# snowboard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# snowboard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with snowboard.  If not, see <http://www.gnu.org/licenses/>.
+
 # A class to store all information the bot knows about a particular channel.
 class Channel:
     def __init__(self, name, members = []):
@@ -10,10 +25,4 @@ class ChannelPriv:
     def __init__(self, isop = False, isvoice = False, level = 0):
         self.op = isop
         self.voice = isvoice
-        self.level = level
-
-# Stpres information about privileges of a nick, across all channels.
-class NickPriv:
-    def __init__(self, level = 0, admin = False):
-        self.admin = admin
         self.level = level
--- snowboard/nick.py
+++ snowboard/nick.py
@@ -1,6 +1,27 @@
+# This file is part of snowboard.
+# 
+# snowboard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# snowboard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with snowboard.  If not, see <http://www.gnu.org/licenses/>.
+
 # Stores information about a nick.
 class Nick:
     def __init__(self, nick, host = "", priv = None):
         self.nick = nick
         self.host = host
         self.priv = priv # NickPriv object
+
+# Stpres information about privileges of a nick, across all channels.
+class NickPriv:
+    def __init__(self, level = 0, admin = False):
+        self.admin = admin
+        self.level = level
